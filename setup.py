89edac39ebde820c1dba9902125200bbdf5c5a3c
Vivek Katial <vivekkatial@gmail.com>
2020-09-10 13:18:32 +1200

    Added init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An implementation for solvinng 3SAT (Exact Cover) using the Quantum Approximate Optimization Algorithm',
+    author='Vivek Katial',
+    license='',
+)
